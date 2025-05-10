9f5fd32cf793a386c82ec5174dd569f0eedbda38
Pedro Moreira <pedro.moreira@emma-sleep.com>
2025-05-10 10:08:30 +0000

    Complete the 8 Bytes Challenge

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	buf := make([]byte, 8)
+
+	for {
+		n, err := f.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// The slice buf[:n] actually means "print the first n bytes that were just read into the buffer"
+		fmt.Printf("read: %s\n", buf[:n])
+	}
 }
