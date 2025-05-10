4c95c3534e747cced0a10098bf8c8efcddacc4bd
Pedro Moreira <pedro.moreira@emma-sleep.com>
2025-05-10 11:40:47 +0000

    Complete the new lines challenge

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
+const inputFilePath = "messages.txt"
+
 func main() {
-	f, err := os.Open("messages.txt")
+	f, err := os.Open(inputFilePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
 	defer f.Close()
 
-	buf := make([]byte, 8)
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	currentLine := ""
 
 	for {
+		buf := make([]byte, 8)
 		n, err := f.Read(buf)
-		if err == io.EOF {
+		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
+
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			fmt.Printf("error: %s\n", err.Error())
 			break
 		}
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		// The slice buf[:n] actually means "print the first n bytes that were just read into the buffer"
-		fmt.Printf("read: %s\n", buf[:n])
+		str := string(buf[:n])
+		parts := strings.Split(str, "\n")
+
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s\n", currentLine+parts[i])
+			currentLine = ""
+		}
+		currentLine += parts[len(parts)-1]
 	}
 }
